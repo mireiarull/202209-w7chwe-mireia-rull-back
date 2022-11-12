eb6cad163042360ea8b25d69bc07413c6a4f6ed4
Mireia Rull <mireia.rullmasdeu@gmail.com>
2022-11-12 08:58:07 +0100

    Add user register controller and router

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,12 +1,22 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
+import userRouter from "./routers/usersRouter.js";
+import { generalError, notFoundError } from "./middlewares/error.js";
 
 const app = express();
+
+app.use(cors());
 
 app.disable("x-powered-by");
 
 app.use(morgan("dev"));
 
 app.use(express.json());
+
+app.use("/users", userRouter);
+
+app.use(notFoundError);
+app.use(generalError);
 
 export default app;
