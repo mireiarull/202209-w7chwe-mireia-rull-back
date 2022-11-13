7cd71ecf5d38f8de43b6aa829002445ef6b36296
Mireia Rull <mireia.rullmasdeu@gmail.com>
2022-11-13 18:44:02 +0100

    Add add relationship controller

---
+++ src/server/controllers/relationshipControllers.ts
@@ -0,0 +1,41 @@
+import type { NextFunction, Request, Response } from "express";
+import CustomError from "../../CustomError/CustomError.js";
+import Relationship from "../../database/models/Relationship.js";
+import type { RegisterRelationship } from "./types";
+
+export const addRelationship = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { user1, user2, relation } = req.body as RegisterRelationship;
+
+  try {
+    await Relationship.create({
+      user1,
+      user2,
+      relation,
+    });
+
+    res.status(201).json(req.body);
+  } catch (error: unknown) {
+    if ((error as Error).message.includes("duplicate")) {
+      const customError = new CustomError(
+        (error as Error).message,
+        409,
+        "Relationship already registered"
+      );
+
+      next(customError);
+      return;
+    }
+
+    const customError = new CustomError(
+      (error as Error).message,
+      500,
+      "Error saving relationship"
+    );
+
+    next(customError);
+  }
+};
--- src/server/controllers/schemas/userCredentialsSchema.ts
+++ src/server/controllers/schemas/userCredentialsSchema.ts
@@ -3,7 +3,7 @@ import { Joi } from "express-validation";
 export const userRegisterSchema = {
   body: Joi.object({
     username: Joi.string().required(),
-    password: Joi.string().min(5).required(),
+    password: Joi.string().required(),
     email: Joi.string().email().required(),
     name: Joi.string().required(),
     job: Joi.string().required(),
